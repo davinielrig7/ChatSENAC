e2ce5a39a676bd9052a146c037991366e1b2dd4a
davinielrig7 <davinielribe3@gmail.com>
2026-07-22 21:39:03 -0300

    criação da tela de login

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,38 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 30.0),
-      ),
-    ),
-  );
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Scaffold(body: Login()));
+  }
+}
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        FlutterLogo(size: 18),
+        Text("ChatSENAC"),
+        Text("Entre na sua conta"),
+        Text("Coloque o seu emaoç e senha para logar"),
+        Text("Email"),
+        TextField(),
+        Text("senha"),
+        TextField(),
+        InkWell(child: Text("Esqueceu a senha?")),
+        ElevatedButton(onPressed: null, child: Text("Entrar")),
+        Text("Ou"),
+        ElevatedButton(onPressed: null, child: Text("Contiuar com o google")),
+        ElevatedButton(onPressed: null, child: Text("Continuar com o facebook")),
+        Text("Não tem uma conta?"),
+        InkWell(child: Text("Cadastre-se")),
+
+      ],
+    );
+  }
 }
