c1c719a005c304b5653942b0c3cbb7dd7083f546
davinielrig7 <davinielribe3@gmail.com>
2026-08-06 21:32:47 -0300

    Utiliza o pacote URI para se conectar ao servidor backend para finalizar o login

--- lib/paginas/cadastro.dart
+++ lib/paginas/cadastro.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:primeiro_app/utilitarios/tipografia.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Cadastro extends StatelessWidget {
   const Cadastro({super.key});
--- lib/paginas/login.dart
+++ lib/paginas/login.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:primeiro_app/paginas/cadastro.dart';
 import 'package:primeiro_app/paginas/dashBoard.dart';
 import 'package:primeiro_app/utilitarios/tipografia.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class Login extends StatefulWidget {
   const Login({super.key});
@@ -14,16 +16,28 @@ class _LoginState extends State<Login> {
   final emailControlador = TextEditingController();
   final senhaControlador = TextEditingController();
 
-  void fazerLogin() {
-    if (emailControlador.text != "teste@email.com" ||
-        senhaControlador.text != "123456") {
+
+
+  Future<void> fazerLogin() async {
+    var url = Uri.http("10.112.4.33", "login");
+    var resposta = await http.post(
+      url,
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: jsonEncode({'email': emailControlador.text, 'senha': senhaControlador.text}),
+    );
+
+    if (resposta.statusCode != 200) {
+      var dados = jsonDecode(resposta.body);
+
       ScaffoldMessenger.of(context).showSnackBar(
-        SnackBar(content: Text("Email e/ou senhas estão incorretos")),
+        SnackBar(content: Text("${dados['message']}")),
       );
 
       return;
     }
-    Navigator.pushReplacement(
+    Navigator.push(
       context,
       MaterialPageRoute(builder: (build) => Dashboard()),
     );
